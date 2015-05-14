1953eb720c7c0795dc4f986a76d52c6ea4d86931
jionsed <feoktet@yahoo.com>
2015-05-14 21:23:46 +0300

    Added parser for forum posts

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,19 @@
-/* Parser, used for debugging, when main page is loaded */
-var poeparser = require('poe-parser');
-
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-	/* Example, how to use thread parser */
-	poeparser.parseThread(
-		"http://web.poe.garena.ru/forum/view-thread/106"
+	res.render(
+		'index',
+		{
+			'title': 'poe trade forums parser'
+		}
 	);
+});
+
+router.post('/', function(req, res, next) {
 	
-	res.render(
-		'index',
-		{
-			'title': 'Ru poe.trade'
-		}
-	);
 });
-
-router.get('/search', function(req, res, next) {
-
-	res.render(
-		'index',
-		{
-			title: 'Ru poe.trade'
-		}
-	);
-});
-
 
 module.exports = router;
